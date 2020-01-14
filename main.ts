d2a0cd1bcddf12812b3d6170ba0470c455fca1ad
paddymcdonnell661 <59517103+paddymcdonnell661@users.noreply.github.com>
2020-01-14 11:35:25 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -80,3 +80,47 @@ d d d d d d d d d d d d d d d d
 d d d d d d d d d d d d d d d d 
 `
 }
+let mySprite: Sprite = null
+scene.cameraFollowSprite(mySprite)
+mySprite = sprites.create(img`
+. . . . . . f f f f . . . . . . 
+. . . . f f f 2 2 f f f . . . . 
+. . . f f f 2 2 2 2 f f f . . . 
+. . f f f e e e e e e f f f . . 
+. . f f e 2 2 2 2 2 2 e e f . . 
+. . f e 2 f f f f f f 2 e f . . 
+. . f f f f e e e e f f f f . . 
+. f f e f b f 4 4 f b f e f f . 
+. f e e 4 1 f d d f 1 4 e e f . 
+. . f e e d d d d d d e e f . . 
+. . . f e e 4 4 4 4 e e f . . . 
+. . e 4 f 2 2 2 2 2 2 f 4 e . . 
+. . 4 d f 2 2 2 2 2 2 f d 4 . . 
+. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+. . . . . f f f f f f . . . . . 
+. . . . . f f . . f f . . . . . 
+`, SpriteKind.tamer)
+controller.moveSprite(mySprite)
+tiles.setTilemap(tiles.createTilemap(
+            hex`1000100007060606060606060706060606060606060607060706060606060607060605050606060606060706060706060605050606060706070606060606060605060606060606060606070607060706060607060607060706060606060606060605050606060606060706060706060505060606060607060606060606070606050507060606060607060506060606060606060606070606060605050506070607060706060606070607060605060606060606060607060606060606060606060505050606060607060607060606070605050506060607060606060607060606060506060706060706070606060706070606070606070606060607060606060606070606`,
+            img`
+2 . . . . . . . 2 . . . . . . . 
+. . 2 . 2 . . . . . . 2 . . . . 
+. . . . . . 2 . . 2 . . . . . . 
+. . 2 . 2 . . . . . . . . . . . 
+. . . . . . 2 . 2 . 2 . . . 2 . 
+. 2 . 2 . . . . . . . . . . . . 
+. . . . . 2 . . 2 . . . . . . . 
+. . 2 . . . . . . 2 . . . . 2 . 
+. . . . 2 . . . . . . . . . . . 
+. 2 . . . . . . . . 2 . 2 . 2 . 
+. . . 2 . 2 . . . . . . . . . . 
+. 2 . . . . . . . . . . . . . . 
+. . . 2 . . 2 . . . 2 . . . . . 
+. . 2 . . . . . 2 . . . . . . . 
+2 . . 2 . 2 . . . 2 . 2 . . 2 . 
+. 2 . . . . 2 . . . . . . 2 . . 
+`,
+            [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,sprites.castle.tileGrass1,sprites.castle.tileDarkGrass3,sprites.castle.tileDarkGrass1,sprites.castle.saplingPine],
+            TileScale.Sixteen
+        ))
