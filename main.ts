cd5cd649b695ed4f374718020e6b4331a05ca3bf
paddymcdonnell661 <59517103+paddymcdonnell661@users.noreply.github.com>
2020-02-23 14:42:25 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -186,24 +186,45 @@ function animateTamer () {
     let tamerDirection = ""
     let tamerState = ""
     if (tamerState == "walking") {
-        mySprite.setImage(img`
-. . . . . . . f f . . . . . . . . . . . 
-. . . . f f f f 2 f f . . . . . . . . . 
-. . f f e e e e f 2 f f . . . . . . . . 
-. f f e e e e e f 2 2 f f . . . . . . . 
-. f e e e e f f e e e e f . . . . . . . 
-. f f f f f e e 2 2 2 2 e f . . . . . . 
-f f f e 2 2 2 f f f f e 2 f . . . . . . 
-f f f f f f f f e e e f f f . . . . . . 
-f e f e 4 4 e b f 4 4 e e f . . . . . . 
-. f e e 4 d 4 b f d d e f . . . . . . . 
-. . f e e e 4 d d d e e . c . . . . . . 
-. . . f 2 2 2 2 e e d d e c c c c c c c 
-. . . f 4 4 4 e 4 4 d d e c d d d d d . 
-. . . f f f f f e e e e . c c c c c . . 
-. . f f f f f f f f . . . c . . . . . . 
-. . f f f . . f f . . . . . . . . . . . 
+        if (mySprite.x % 2 == 0) {
+            mySprite.setImage(img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . f f e e e e f 2 f . . . . 
+. . f f e e e e f 2 2 2 f . . . 
+. . f e e e f f e e e e f . . . 
+. . f f f f e e 2 2 2 2 e f . . 
+. . f e 2 2 2 f f f f e 2 f . . 
+. f f f f f f f e e e f f f . . 
+. f f e 4 4 e b f 4 4 e e f . . 
+. f e e 4 d 4 1 f d d e f . . . 
+. . f e e e e e d d d f . . . . 
+. . . . f 4 d d e 4 e f . . . . 
+. . . . f e d d e 2 2 f . . . . 
+. . . f f f e e f 5 5 f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . . f f . . . f f f . . . . 
 `)
+        } else {
+            mySprite.setImage(img`
+. . . . . . f f f f f f . . . . 
+. . . . f f e e e e f 2 f . . . 
+. . . f f e e e e f 2 2 2 f . . 
+. . . f e e e f f e e e e f . . 
+. . . f f f f e e 2 2 2 2 e f . 
+. . . f e 2 2 2 f f f f e 2 f . 
+. . f f f f f f f e e e f f f . 
+. . f f e 4 4 e b f 4 4 e e f . 
+. . f e e 4 d 4 1 f d d e f . . 
+. . . f e e e 4 d d d d f . . . 
+. . . . f f e e 4 4 4 e f . . . 
+. . . . . 4 d d e 2 2 2 f . . . 
+. . . . . e d d e 2 2 2 f . . . 
+. . . . . f e e f 4 5 5 f . . . 
+. . . . . . f f f f f f . . . . 
+. . . . . . . f f f . . . . . . 
+`)
+        }
     }
     if (tamerState == "attacking") {
         mySprite.setImage(img`
